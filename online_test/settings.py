09e5ae949b7bb2b10aee01e41994e2d71075b7ac
Gaurav <gauravgahlyan27@gmail.com>
2020-08-16 17:26:17 +0530

    whitenoise

--- online_test/settings.py
+++ online_test/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'test_app' / 'static'
+# STATIC_ROOT = BASE_DIR / 'test_app' / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -127,3 +128,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
 
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
